ea735f73c1fb6aaab79979209c7663005d127136
nickgrief <wotniga@gmail.com>
2022-06-29 17:27:44 +0300

    Add home page

---
+++ src/home/home.ts
@@ -0,0 +1,56 @@
+import * as index from '../index';
+import icImage from './icecream.jpg';
+
+const bp = (point: string, color: string) => {
+  return {
+    point,
+    color,
+  };
+};
+
+const bps = [
+  bp('Sugar and milk? What more can I ask?', 'deeppink'),
+  bp("If you're cute come right in!", 'mediumseagreen'),
+  bp("If you're not... You're cute!", 'orange'),
+  bp(
+    'So, just come here and eat icecream. Nothing can go wrong...',
+    'indianred'
+  ),
+  bp(
+    'JoJo is the best anime. If you agree then come eat icecream with me!',
+    'fuchsia'
+  ),
+  bp(
+    'Anyway... I need to fill some space. BOBOBOBOLLALALALKEKEKEKKERARARARA',
+    'indigo'
+  ),
+];
+
+export function show() {
+  index.createHeader();
+  createMain();
+}
+
+function createMain() {
+  const main = document.createElement('main');
+
+  const icecream = document.createElement('img');
+  icecream.classList.toggle('icecream');
+  icecream.src = icImage;
+  icecream.title = 'Photo by Benjamín Hazael Rojas García';
+  main.appendChild(icecream);
+
+  // I've had a stroke while naming things. Let's just never look here m'kay?
+  const bullet = document.createElement('ul');
+  bullet.classList.toggle('bullet-points');
+  for (let p of bps) {
+    const bpoint = document.createElement('li');
+    bpoint.classList.toggle('bullet-point');
+    bpoint.textContent = p.point;
+    bpoint.style.color = p.color;
+    bullet.appendChild(bpoint);
+  }
+  main.appendChild(bullet);
+
+  index.body.appendChild(main);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
 import './reset.css';
 import './style.css';
 
-const body = document.querySelector('body');
-const hello = document.createElement('h1');
-hello.textContent = 'Hello!';
-body?.appendChild(hello);
+import * as home from './home/home';
+
+const tabNames = ['Home', 'Menu', 'Contacts'];
+
+export const body = document.querySelector('body')!;
+
+home.show();
+
+export function createHeader() {
+  const header = document.createElement('header');
+  // header.classList.toggle('header');
+
+  const name = document.createElement('h1');
+  name.classList.toggle('name');
+  name.textContent = 'Ice Cweam De-luxe :3';
+  header.appendChild(name);
+
+  const slogan = document.createElement('p');
+  slogan.classList.toggle('slogan');
+  slogan.textContent = '=^..^= Be cute or be cuten =^..^=';
+  header.appendChild(slogan);
+
+  const tabs = document.createElement('ul');
+  tabs.classList.toggle('tab-list');
+  for (let tabName of tabNames) {
+    const tab = document.createElement('li');
+    tab.classList.toggle('tab-list-item');
+    const link = document.createElement('button');
+    link.textContent = tabName;
+    tab.appendChild(link);
+    tabs.appendChild(tab);
+  }
+  header.appendChild(tabs);
+
+  body.appendChild(header);
+}
